84b219cf8700ef4f742f0cd03cdb8bf740313e6b
hhhyde <333333aa>
2016-10-21 17:25:42 +0800

    hhhh

--- src/main/java/redisdemo/RedisDemo.java
+++ src/main/java/redisdemo/RedisDemo.java
@@ -13,9 +13,27 @@ public class RedisDemo {
 		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
 		InputStream is = classloader.getResourceAsStream("secure.properties");
 		prop.load(is);
-		Jedis jedis = new Jedis(prop.getProperty("redis_host"));
+
+		Jedis jedis = new Jedis(prop.getProperty("redis_host"), 6380);
 		jedis.auth(prop.getProperty("redis_pass"));
-		String value = jedis.get("foo");
+		String value = jedis.get("foo6");
 		System.out.println(value);
+
+		// TODO: 2016/10/21 集群暂时有问题,不能给定密码
+		//GenericObjectPoolConfig pool = new GenericObjectPoolConfig();
+		//pool.setMaxIdle(5);
+		//pool.setMaxTotal(1024);
+		//pool.setMaxWaitMillis(5000);
+		//pool.setTestOnBorrow(true);
+		//
+		//HostAndPort node1= new HostAndPort(prop.getProperty("redis_host"),6379);
+		//HostAndPort node2= new HostAndPort(prop.getProperty("redis_host"),6380);
+		//Set<HostAndPort> nodeSet = new HashSet<HostAndPort>();
+		//nodeSet.add(node1);
+		//nodeSet.add(node2);
+		//JedisCluster cluster = new JedisCluster(nodeSet,1000,1000,1000,"foobared",pool);
+		//cluster.set("foo","bar2");
+		//System.out.println(cluster.get("foo2"));
+
 	}
 }
