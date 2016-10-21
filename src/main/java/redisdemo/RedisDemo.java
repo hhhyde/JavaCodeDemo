cc92e60e85cbfbf6657eff0b90cef4e7370cb665
hhhyde <333333aa>
2016-10-21 10:16:30 +0800

    hhhh

--- src/main/java/redisdemo/RedisDemo.java
+++ src/main/java/redisdemo/RedisDemo.java
@@ -2,13 +2,19 @@ package redisdemo;
 
 import redis.clients.jedis.Jedis;
 
-/**
- * Created by Administrator on 2016/10/20.
- */
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class RedisDemo {
-	public static void main(String[] args) {
-		Jedis jedis = new Jedis("host");
-		jedis.auth("password");
+	public static void main(String[] args) throws IOException {
+		Properties prop = new Properties();
+		//maven项目中resources目录下就是classpath下。
+		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+		InputStream is = classloader.getResourceAsStream("secure.properties");
+		prop.load(is);
+		Jedis jedis = new Jedis(prop.getProperty("redis_host"));
+		jedis.auth(prop.getProperty("redis_pass"));
 		String value = jedis.get("foo");
 		System.out.println(value);
 	}
