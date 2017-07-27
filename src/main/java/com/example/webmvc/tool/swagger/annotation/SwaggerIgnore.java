6c4ea0b3f9dc21f853a897639485d71f910023da
hhhyde <333333>
2017-07-27 14:05:36 +0800

    Swagger 忽略节点

--- src/main/java/com/example/webmvc/tool/swagger/annotation/SwaggerIgnore.java
+++ src/main/java/com/example/webmvc/tool/swagger/annotation/SwaggerIgnore.java
@@ -6,9 +6,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Created by kke on 2017-07-15.
- */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SwaggerIgnore {
--- src/main/java/com/example/webmvc/web/DemoApplication.java
+++ src/main/java/com/example/webmvc/web/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.webmvc.web;
 
+import com.example.webmvc.tool.swagger.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Import({SwaggerConfig.class})
 @SpringBootApplication
 @EnableSwagger2
 @ComponentScan("com.example.webmvc.web")
